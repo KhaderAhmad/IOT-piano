73263b0a11c1ae23b21fdc646c0ceda1cb3fd76b
sirhanadan <107433544+sirhanadan@users.noreply.github.com>
2024-08-08 17:32:34 +0300

    Update add_task.dart - working

--- add_task.dart
+++ add_task.dart
@@ -1,40 +1,170 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
-import 'screens/signin_screen.dart';
+class AddSong extends StatefulWidget {
+  final String userName;
+  final Map<String, dynamic> songs;
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  try {
-    await Firebase.initializeApp();
-  } catch (e) {
-    print('Error initializing Firebase: $e');
-  }
-  runApp(MyApp());
+  const AddSong({
+    super.key,
+    required this.userName,
+    required this.songs,
+  });
+
+  @override
+  State<AddSong> createState() => _AddSongState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _AddSongState extends State<AddSong> {
+  final TextEditingController _songNameController = TextEditingController();
+  late DatabaseReference recordedRef;
+  late DatabaseReference durationRef;
+  late Stream<DatabaseEvent> recordedStream;
+  late Stream<DatabaseEvent> durationStream;
 
-  // This widget is the root of your application.
+  String? recordedData;
+  String? durationData;
+
+  @override
+  void initState() {
+    super.initState();
+    _updateCurrentModeToRecord();
+
+    String userId = FirebaseAuth.instance.currentUser!.uid;
+
+    recordedRef = FirebaseDatabase.instance.ref('/recorded');
+    durationRef = FirebaseDatabase.instance.ref('/duration');
+
+    recordedStream = recordedRef.onValue;
+    durationStream = durationRef.onValue;
+
+    _listenToRecordedChanges();
+    _listenToDurationChanges();
+  }
+
+  @override
+  void dispose() {
+    _updateCurrentModeToFreePlay();
+    super.dispose();
+  }
+
+  void _updateCurrentModeToRecord() async {
+    String userId = FirebaseAuth.instance.currentUser!.uid;
+
+    // Update in Firestore
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(userId)
+        .update({'currentMode': 'record'});
+
+    // Update in Realtime Database
+    await FirebaseDatabase.instance
+        .reference()
+        .update({'currentMode': 'record'});
+  }
+
+  void _updateCurrentModeToFreePlay() async {
+    String userId = FirebaseAuth.instance.currentUser!.uid;
+
+    // Update in Firestore
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(userId)
+        .update({'currentMode': 'freePlay'});
+
+    // Update in Realtime Database
+    await FirebaseDatabase.instance
+        .reference()
+        .update({'currentMode': 'freePlay'});
+  }
+
+  void _listenToRecordedChanges() {
+    recordedStream.listen((DatabaseEvent event) async {
+      setState(() {
+        recordedData = event.snapshot.value.toString();
+      });
+      await _updateFirestoreWithSongData();
+    });
+  }
+
+  void _listenToDurationChanges() {
+    durationStream.listen((DatabaseEvent event) async {
+      setState(() {
+        durationData = event.snapshot.value.toString();
+      });
+      await _updateFirestoreWithSongData();
+    });
+  }
+
+  Future<void> _updateFirestoreWithSongData() async {
+    String songName = _songNameController.text;
+
+    if (songName.isNotEmpty) {
+      Map<String, dynamic> songData = {};
+      if (recordedData != null) songData['notes'] = recordedData;
+      if (durationData != null) songData['duration'] = durationData;
+
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(FirebaseAuth.instance.currentUser!.uid)
+          .collection('songs')
+          .doc(songName)
+          .set(songData, SetOptions(merge: true)); // Merge to update only specific fields
+    } else {
+      _showErrorMessage('Please insert a name for your song before recording.');
+    }
+  }
+
+  void _showErrorMessage(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        duration: const Duration(seconds: 4),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Add a new song for ${widget.userName}',
+          style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Plus Jakarta Sans'),
+        ),
       ),
-      home: const SignInScreen(),
-      debugShowCheckedModeBanner: false,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: _songNameController,
+                decoration: InputDecoration(
+                  labelText: 'Song Name',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                "Please enter a name then press the start button to start recording",
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.normal,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
